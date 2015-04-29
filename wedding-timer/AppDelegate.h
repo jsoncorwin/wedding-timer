96a9b5316b700628ede75ad6d7b514be9dc72701
Jason Corwin <jason.corwin@spandigital.com>
2015-04-29 14:08:14 -0700

    Initial Commit

---
+++ wedding-timer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wedding-timer
+//
+//  Created by Jason Corwin on 4/29/15.
+//  Copyright (c) 2015 Jason M Corwin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ wedding-timer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  wedding-timer
+//
+//  Created by Jason Corwin on 4/29/15.
+//  Copyright (c) 2015 Jason M Corwin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
